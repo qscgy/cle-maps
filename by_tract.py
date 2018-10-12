90592a54035143817134db0f737bd260ad9fd0fb
qscgy <sam.ehrenstein@gmail.com>
2018-10-12 14:45:48 -0400

    idk

--- by_tract.py
+++ by_tract.py
@@ -14,14 +14,6 @@ def add_basemap(ax, zoom, url='http://tile.stamen.com/terrain/tileZ/tileX/tileY.
     ax.axis((xmin, xmax, ymin, ymax))
 
 
-def add_basemap(ax, zoom, url='http://tile.stamen.com/terrain/tileZ/tileX/tileY.png'):
-    xmin, xmax, ymin, ymax = ax.axis()
-    basemap, extent = ctx.bounds2img(xmin, ymin, xmax, ymax, zoom=zoom, url=url)
-    ax.imshow(basemap, extent=extent, interpolation='bilinear')
-    # restore original x/y limits
-    ax.axis((xmin, xmax, ymin, ymax))
-
-
 # converts tract number to 6-character FIPS string
 def format_geoid(g):
     if len(g)==4:
@@ -34,17 +26,22 @@ def format_geoid(g):
 
 data_col = 'lead_pct'
 
+cle_tracts = pd.read_csv('cuyahoga_tracts.csv')
+cle_tracts = cle_tracts[cle_tracts['placenme'] == 'Cleveland city']
+
 race = pd.read_csv('white_pct.csv', skiprows=3)
 race['pct_nonwhite'] = 100-race['pct_white']
+race['pct_nonwhite'] = race['pct_nonwhite'].map(lambda x: max(0, x))
 
 lead_levels = pd.read_csv("lead_census_tracts_cuyahoga.csv", skiprows=3)    # lead level data
 lead_levels[data_col] = lead_levels[data_col].map(lambda x: max(0, x))
-lead_levels.merge(race, on='Census Tract')
+lead_levels = pd.merge(lead_levels, race, on='Census Tract')
+# lead_levels = lead_levels[lead_levels['Census Tract'].isin(cle_tracts['tract'])]
 print(lead_levels.head())
 
 map_df = gpd.read_file("tl_2015_39_tract.shp")
 # print(map_df.crs)
-tract_ids = lead_levels.iloc[:,1]*100
+tract_ids = lead_levels['Census Tract']*100
 
 geoids = []
 for i in tract_ids:
@@ -55,7 +52,10 @@ lead_levels['GEOID'] = geoids
 # print(lead_levels)
 
 map_df = map_df[map_df['GEOID'].isin(geoids)]   # select only the tracts that are in the lead level dataset
-map_df = map_df.cx[-81.8:-81.5, 41.4:41.6]  # approximate Cleveland bounding box
+print(map_df['GEOID'])
+fips = [str(g)[:-1] for g in cle_tracts['stfid00_bg']]
+map_df = map_df[map_df['GEOID'].isin(fips)]
+# map_df = map_df.cx[-81.8:-81.5, 41.4:41.6]  # approximate Cleveland bounding box
 map_df = map_df.merge(lead_levels, on='GEOID')
 # map_df = map_df.to_crs(epsg=3857)
 map_df.head()
@@ -68,9 +68,13 @@ g = gpd.GeoSeries([cc])
 g.crs = map_df.crs
 g.plot(ax=ax, color='red')
 # ax.plot(-81.62, 41.5, color='red', markersize=1000)
-plt.title('Number of children under 6 with elevated blood lead levels')
+plt.title('Percent of children under 6 with elevated blood lead levels')
 
-plt.figure()
+# plt.figure()
 # print(list(map_df))
 ax2 = map_df.plot(column='pct_nonwhite', figsize=(20, 16), legend=True)
+ax2.set_title('Percent of population who are not white')
+
+ax3 = map_df.plot()
+
 plt.show()
---
+++ correlations.R
@@ -0,0 +1,5 @@
+library(ggplot2)
+
+# Lead vs. race
+lead <- read.csv("lead_census_tracts_cuyahoga.csv", skip = 3)
+race
